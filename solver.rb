805cc68890559040a065f4fcfae07d654bf0cf5a
yodit93 <yoditabebe9391@gmail.com>
2023-05-25 17:32:25 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,27 +1,30 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, "Number must be positive" if n < 0
-        return 1 if n == 0
-        return n * factorial(n - 1)
-    end
+  def factorial(num)
+    raise ArgumentError, 'Number must be positive' if num.negative?
+    return 1 if num.zero?
 
-    def reverse(str)
-        new_str = ""
-        return nil if str.strip == ""
-        (str.length - 1).downto(0) do |index|
-            new_str += str[index]
-        end
-        new_str
+    num * factorial(num - 1)
+  end
+
+  def reverse(str)
+    new_str = ''
+    return nil if str.strip == ''
+
+    (str.length - 1).downto(0) do |index|
+      new_str += str[index]
     end
-    def fizz_buzz(num)
-        if num % 3 == 0 && num % 5 == 0
-            "fizzbuzz"
-        elsif num % 3 == 0
-            "fizz"
-        elsif num % 5 == 0
-            "buzz"
-        else 
-            num.to_s
-        end
+    new_str
+  end
+
+  def fizz_buzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
+  end
 end
