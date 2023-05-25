83d087a32d270bf55000464056b70c996b427af7
yodit93 <yoditabebe9391@gmail.com>
2023-05-25 13:35:58 +0300

    Create and add reverse method to solver class

--- solver.rb
+++ solver.rb
@@ -4,4 +4,12 @@ class Solver
         return 1 if n == 0
         return n * factorial(n - 1)
     end
+
+    def reverse(str)
+        new_str = ""
+        (str.length - 1).downto(0) do |index|
+            new_str += str[index]
+        end
+        new_str
+    end
 end
