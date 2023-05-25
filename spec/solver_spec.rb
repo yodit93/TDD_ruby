ffbc9a79f3108ba933e636417f4a2614ad55afb8
yodit93 <yoditabebe9391@gmail.com>
2023-05-25 13:18:39 +0300

    add test for raising exeption error for negative numbers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,15 @@
 require_relative '../solver'
 describe Solver do
+    before(:all) do
+        @solver = Solver.new
+    end
     describe "#factorial" do
         it "should return the factorial of the number" do
-            solver = Solver.new
-            expect(solver.factorial(4)).to eq(24)
+            expect(@solver.factorial(4)).to eq(24)
+        end
+
+        it "should raise an exeption when it recieves negative number" do
+            expect{@solver.factorial(-4)}.to raise_error(ArgumentError, "Number must be positive")
         end
     end
 end
