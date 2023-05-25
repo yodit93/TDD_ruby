d00daafcfad9336af7dbb20f8fd13869851ff620
yodit93 <yoditabebe9391@gmail.com>
2023-05-25 17:49:02 +0300

    Add factorial test for num = 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,6 +8,10 @@ describe Solver do
       expect(@solver.factorial(4)).to eq(24)
     end
 
+    it 'should return the factorial of the number' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
     it 'should raise an exeption when it recieves negative number' do
       expect { @solver.factorial(-4) }.to raise_error(ArgumentError, 'Number must be positive')
     end
